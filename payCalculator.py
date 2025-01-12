da08cb3e0a5eb72e864138cdd5360c43676f61f7
Morgan <morgan.williams@vu.com>
2025-01-11 21:20:07 -0600

    pay

--- payCalculator.py
+++ payCalculator.py
@@ -3,6 +3,10 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
+    rate = input("Enter Rate:")
+    pay = hrs * rate
+    print(pay)
+
 
     # end assignment
 
